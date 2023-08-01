04ad6c9ca04c7fc97045b24c6686c72a7e24e22a
Phuc Tran <tptran.sw@gmail.com>
2023-08-01 23:07:06 +0200

    support semi ui 2.4.0

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # vite-plugin-semi-theme
+This is a fork from https://github.com/boenfu/vite-plugin-semi-theme
 
-- [theme options docs](https://github.com/DouyinFE/semi-design/tree/main/packages/semi-webpack#api)
-
-```js
-// vite.config.js
-import { defineConfig } from "vite";
-import semi from "vite-plugin-semi-theme";
-
-export default defineConfig({
-  plugins: [
-    semi({
-      theme: "@semi-bot/semi-theme-yours",
-      // options: {
-      // ... 👆
-      //},
-    }),
-  ],
-});
-```
--- index.js
+++ index.js
@@ -79,61 +79,62 @@ module.exports = function ({ theme, options = {} }) {
 
 // copy from https://github.com/DouyinFE/semi-design/blob/main/packages/semi-webpack/src/semi-theme-loader.ts
 function loader(source, options) {
-  let fileStr = source.toString("utf8");
-
-  const theme = options.name || "@douyinfe/semi-theme-default";
+  const query = loaderUtils.getOptions ? loaderUtils.getOptions(this) : loaderUtils.parseQuery(this.query);
+  const theme = query.name || '@douyinfe/semi-theme-default';
   // always inject
   const scssVarStr = `@import "~${theme}/scss/index.scss";\n`;
   // inject once
   const cssVarStr = `@import "~${theme}/scss/global.scss";\n`;
-  // [vite-plugin]: sync from https://github.com/DouyinFE/semi-design/commit/a6064489a683495a737cbe7abd72c0b49a3bcd06
   let animationStr = `@import "~${theme}/scss/animation.scss";\n`;
 
   try {
-    require.resolve(`${theme}/scss/animation.scss`);
+      resolve.sync(this.context, `${theme}/scss/animation.scss`);
   } catch (e) {
-    animationStr = ""; // fallback to empty string
+      animationStr = ""; // fallback to empty string
   }
 
-  const shouldInject = fileStr.includes("semi-base");
+
+  const shouldInject = source.includes('semi-base');
+
+  let fileStr = source;
 
   let componentVariables;
-
   try {
-    componentVariables = require.resolve(`${theme}/scss/local.scss`);
-  } catch (e) {}
+      componentVariables = resolve.sync(this.context, `${theme}/scss/local.scss`);
+  } catch (e) {
+  }
 
-  if (options.include || options.variables || componentVariables) {
-    let localImport = "";
-    if (componentVariables) {
-      localImport += `\n@import "~${theme}/scss/local.scss";`;
-    }
-    if (options.include) {
-      localImport += `\n@import "${options.include}";`;
-    }
-    if (options.variables) {
-      localImport += `\n${options.variables}`;
-    }
-    try {
-      const regex =
-        /(@import '.\/variables.scss';?|@import ".\/variables.scss";?)/g;
-      const fileSplit = fileStr.split(regex).filter((item) => Boolean(item));
-      if (fileSplit.length > 1) {
-        fileSplit.splice(fileSplit.length - 1, 0, localImport);
-        fileStr = fileSplit.join("");
+  if (query.include || query.variables || componentVariables) {
+      let localImport = '';
+      if (componentVariables) {
+          localImport += `\n@import "~${theme}/scss/local.scss";`;
       }
-    } catch (error) {}
+      if (query.include) {
+          localImport += `\n@import "${query.include}";`;
+      }
+      if (query.variables) {
+          localImport += `\n${query.variables}`;
+      }
+      try {
+          const regex = /(@import '.\/variables.scss';?|@import ".\/variables.scss";?)/g;
+          const fileSplit = source.split(regex).filter(item => Boolean(item));
+          if (fileSplit.length > 1) {
+              fileSplit.splice(fileSplit.length - 1, 0, localImport);
+              fileStr = fileSplit.join('');
+          }
+      } catch (error) {
+      }
   }
 
   // inject prefix
-  const prefixCls = options.prefixCls || "semi";
+  const prefixCls = query.prefixCls || 'semi';
 
   const prefixClsStr = `$prefix: '${prefixCls}';\n`;
 
   if (shouldInject) {
-    return `${animationStr}${cssVarStr}${scssVarStr}${prefixClsStr}${fileStr}`;
+      return `${animationStr}${cssVarStr}${scssVarStr}${prefixClsStr}${fileStr}`;
   } else {
-    return `${scssVarStr}${prefixClsStr}${fileStr}`;
+      return `${scssVarStr}${prefixClsStr}${fileStr}`;
   }
 }
 
